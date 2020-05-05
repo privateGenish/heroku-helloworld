cdf8a22cb7442af87a6c48e33e7665f3b2155baa
therealgenish <yoavg111@gmail.com>
2020-05-05 13:07:55 +0300

    fuck

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const app = require('fastify')();
+const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -6,7 +8,8 @@ app.listen(PORT, function(){
     console.log("app is listening to port: " + PORT);
 });
 var helloWorld = async function(req,replay){
-    await replay.send({body: "Hello World"}).code(200);
+    const stream = fs.createReadStream("./helloWorld.html")
+    await replay.type('text/html').send(stream).code(200);
     console.log("Hello World: " + replay.statusCode);
 }
 app.get("/", helloWorld)
